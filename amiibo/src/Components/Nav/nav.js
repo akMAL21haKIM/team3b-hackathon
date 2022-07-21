3e3db06f2f2cc2ab943d7f36aca0d3b643f65c92
hassif <hassif@besquare.com.my>
2022-07-21 16:10:11 +0800

    css

--- amiibo/src/Components/Nav/nav.js
+++ amiibo/src/Components/Nav/nav.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
 import logo from "../Home/asset/logo3.png";
 
 const Nav = () => {
+  const ref = useRef();
   return (
     <div>
       <div className="grid-container">
-        <div className="grid-item">
+        <div className="grid-item-logo">
           <div className="logo">
             <Link to="/">
               <img src={logo} />
             </Link>
           </div>
         </div>
-        <div className="grid-item">
-          <div className="menu-buttons">
+        <div className="grid-item" id="grid-item-menu">
+          <div
+            className="grid-item__icon"
+            onClick={() => {
+              // debugger;
+              ref.current.classList.toggle("visible");
+            }}
+          >
+            <div className="menu-icon"></div>
+            <div className="menu-icon"></div>
+            <div className="menu-icon"></div>
+          </div>
+          <div className="menu-buttons" ref={ref}>
             <Link to="/">
               <button id="home-btn">Home</button>
             </Link>
@@ -30,6 +42,7 @@ const Nav = () => {
       </div>
     </div>
   );
+  <script></script>;
 };
 
 export default Nav;
